5da2e689bbc84b7462ba6070097e87bb875de5cd
Nicholas Skupien <nicholasskupien@gmail.com>
2021-04-27 12:41:40 -0400

    adding starter files
    tonc demo and template for my own version of the demo

---
+++ obj_demo_cat/cat3.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(cat3)
+
+//======================================================================
+//
+//	cat3, 32x32@4, 
+//	+ palette 10 entries, not compressed
+//	+ 16 tiles not compressed
+//	Total size: 20 + 512 = 532
+//
+//	Time-stamp: 2021-04-23, 00:38:05
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_CAT3_H
+#define GRIT_CAT3_H
+
+#define cat3TilesLen 512
+extern const unsigned int cat3Tiles[128];
+
+#define cat3PalLen 20
+extern const unsigned int cat3Pal[5];
+
+#endif // GRIT_CAT3_H
+
+//}}BLOCK(cat3)
